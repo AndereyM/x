5a66095250162a34d42fbd5ebffd50db08250feb
AndereyM <142475228+AndereyM@users.noreply.github.com>
2023-08-25 10:46:58 +0700

    Update some_app.py

--- lab5_web/flaskapp/some_app.py
+++ lab5_web/flaskapp/some_app.py
@@ -9,6 +9,56 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
+
+
+
+
+from flask import request
+from flask import Response 
+83
+import base64
+from PIL import Image
+from io import BytesIO
+import json
+# метод для обработки запроса от пользователя 
+@app.route("/apinet",methods=['GET', 'POST'])
+def apinet():
+ neurodic = {} 
+ # проверяем, что в запросе json данные
+ if request.mimetype == 'application/json': 
+ # получаем json данные
+ data = request.get_json()
+ # берем содержимое по ключу, где хранится файл 
+ # закодированный строкой base64
+ # декодируем строку в массив байт, используя кодировку utf-8
+ # первые 128 байт ascii и utf-8 совпадают, потому можно
+ filebytes = data['imagebin'].encode('utf-8') 
+ # декодируем массив байт base64 в исходный файл изображение
+ cfile = base64.b64decode(filebytes) 
+ # чтобы считать изображение как файл из памяти, используем BytesIO
+ img = Image.open(BytesIO(cfile))
+ decode = neuronet.getresult([img])
+ neurodic = {}
+ for elem in decode:
+ neurodic[elem[0][1]] = str(elem[0][2])
+ print(elem)
+ # пример сохранения переданного файла
+ # handle = open('./static/f.png','wb')
+ # handle.write(cfile)
+ # handle.close()
+ # преобразуем словарь в json-строку
+ ret = json.dumps(neurodic) 
+ # готовим ответ пользователю
+ resp = Response(response=ret,
+ status=200, 
+ mimetype="application/json") 
+ # возвращаем ответ
+ return resp 
+
+
+
+
+
 #наша новая функция сайта
 @app.route("/data_to") 
 def data_to():
